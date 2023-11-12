0b52330633408c77f5eab01babbaeb0d14d263b7
Eyenndaeyo <ndaeyoelijah@gmail.com>
2023-11-12 22:03:22 +0100

    elijahs login page

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 2000,
+     
+    // Navigation arrows
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+  
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 1024px
+      1000: {
+        slidesPerView: 3,
+        spaceBetween: 50
+      },
+    },
+    autoplay: {
+      delay: 5000,
+    },
+   
+  });
